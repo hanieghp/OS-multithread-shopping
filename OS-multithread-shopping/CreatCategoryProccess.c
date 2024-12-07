309cae079964e6b1beafbc9903efb0c57fe8ec03
Bahar Mohamadi Sani <baharmohamadisani83@gmail.com>
2024-12-07 19:30:39 +0330

    function for finding directories added

--- OS-multithread-shopping/CreatCategoryProccess.c
+++ OS-multithread-shopping/CreatCategoryProccess.c
@@ -37,12 +37,70 @@ char* categories[] = {
     Sleep(i * 1000);
     printf("I'm process number %d\n", i);
     return 0;
+}*/
+
+UserData *processShoppingList () {
+    UserData *userData = malloc(sizeof(UserData));
+    if (userData == NULL) {
+        perror("Failed to allocate memory\n");
+        return 1;
+    }
+
+    printf("Enter user ID (or 'exit' to quit): ");
+    scanf("%s", userData->userID);
+    if (strcmp(userData->userID, "exit") == 0) {
+        free(userData);
+        return 0;
+    }
+
+    printf("Enter budget cap (if you have no cap enter -1): ");
+    scanf("%lf", &userData->budgetCap);
+
+    printf("Enter number of products: ");
+    scanf("%d", &userData->productCount);
+
+    /*if (userData->productCount > MAX_PRODUCTS) {
+        printf("Too many products! Maximum number of product allowed is %d.\n", MAX_PRODUCTS);
+        free(userData);
+        continue;
+    }*/
+
+    for (int i = 0; i < userData->productCount; i++) {
+        printf("  Enter product %d Name: ", i + 1);
+        scanf("%s", userData->products[i].name);
+
+        printf("  Enter product %d Quantity: ", i + 1);
+        scanf("%d", &userData->products[i].quantity);
+
+        printf("  Enter product %d Price: ", i + 1);
+        scanf("%lf", &userData->products[i].price);
+    }
+    printf("Processing shopping list for user: %s\n", userData->userID);
+
+    return userData;
 }
 
-/*DWORD WINAPI processShoppingList(LPVOID arg) {
-    UserData *data = (UserData *)arg;
-
-    printf("Processing shopping list for user: %s\n", data->userID);
-
-    // need to implement
-}*/
+char** getSubDirectories(char dir[1000]){
+    int count = 0;
+    char **categories = malloc(sizeof(char *) * 1000);       
+    char sdir[1000], command[1000];
+    printf("im in\n");
+    snprintf(command, sizeof(command), "find %s -maxdepth 1 -type d", dir);
+    printf("im in\n");
+    FILE *fp = popen((command), "r");
+    printf("im in\n");
+    
+    if (!fp) {
+        perror("Error opening files");
+        if (fp) fclose(fp);
+        return;
+    }
+    fgets(sdir, sizeof(sdir), fp);
+    while(fgets(sdir, sizeof(sdir), fp)!=NULL){
+        printf("%s", sdir);
+        categories[count] = strdup(sdir);
+        count++;                 
+    }
+    pclose(fp);
+    return categories;
+}
--- main.c
+++ main.c
@@ -2,64 +2,41 @@
 #include "OS-multithread-shopping/CreatCategoryProccess.c" 
 
 int main(){
-while (1) {
-        UserData *userData = malloc(sizeof(UserData));
-        if (userData == NULL) {
-            perror("Failed to allocate memory\n");
-            return 1;
-        }
-
-        printf("Enter user ID (or 'exit' to quit): ");
-        scanf("%s", userData->userID);
-        if (strcmp(userData->userID, "exit") == 0) {
-            free(userData);
-            break;
-        }
-
-        printf("Enter budget cap (if you have no cap enter -1): ");
-        scanf("%lf", &userData->budgetCap);
-
-        printf("Enter number of products: ");
-        scanf("%d", &userData->productCount);
-
-        if (userData->productCount > MAX_PRODUCTS) {
-            printf("Too many products! Maximum number of product allowed is %d.\n", MAX_PRODUCTS);
-            free(userData);
-            continue;
-        }
-
-        for (int i = 0; i < userData->productCount; i++) {
-            printf("  Enter product %d Name: ", i + 1);
-            scanf("%s", userData->products[i].name);
-
-            printf("  Enter product %d Quantity: ", i + 1);
-            scanf("%d", &userData->products[i].quantity);
-
-            printf("  Enter product %d Price: ", i + 1);
-            scanf("%lf", &userData->products[i].price);
-        }
-
+    while (1) {
         pid_t pidUser = vfork();
         
         if(pidUser < 0){
             printf("fork faild!\n");
         }
 
-        if(pidUser == 0){
+        if(pidUser == 0){ 
             int storeCount = 3;
+            char Dataset[1000] = "Dataset";  
+            char** stores = getSubDirectories(Dataset);
+
+            //storeCount = sizeof(stores);
+            for (int i = 0; i < storeCount; i++){
+                printf("k : %s\n", stores[i]);
+            }
+            UserData *userData = malloc(sizeof(UserData));
+            userData = processShoppingList();
+            
             // making process for stores
+            
             for (int i = 0; i < storeCount; i++){
                 pid_t pidStore = vfork();
                 if(pidStore == 0){
+                    char dir[1000] = "";
                     printf("im searching for your items in store%d\n", i);
-                    
+                    //creating childs fot categories
+                    char ** categories;
+                    categories = getSubDirectories(stores[i]);
                 }
                 if(pidStore < 0){
                     printf("make process for store number %d faild\n", i);
                 }
             }
         }
-
     }
     printf("exit.\n");
 
