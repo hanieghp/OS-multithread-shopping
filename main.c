9e75bae207985d163bf8d498aa033481a5689cc6
Bahar Mohamadi Sani <baharmohamadisani83@gmail.com>
2024-12-16 11:14:41 +0330

    shared memory added

--- main.c
+++ main.c
@@ -14,6 +14,9 @@
 #include <bits/pthreadtypes.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
 
 #define FILESNUMBER 79
 #define MAX_PRODUCTS 80
@@ -60,6 +63,7 @@ typedef struct {
   int purchaseCount[MAX_storeCount];
   bool hasDiscount[MAX_storeCount];
   int entity[MAX_PRODUCTS];
+  int shmFd;
 } UserShoppingList;
 
 typedef struct {
@@ -77,6 +81,8 @@ sem_t *g_result_sem = NULL;
 sem_t *g_rating_sem = NULL;
 sem_t *g_shopping_list_sem = NULL;
 
+UserShoppingList* shoppingList;
+int *userCount = 0;
 pthread_mutex_t liveLock = PTHREAD_MUTEX_INITIALIZER;
 bool stopThread = false;
 
@@ -184,7 +190,7 @@ Product* readProductFromFile(const char* filepath) {
 }
 
 // Function to read user's shopping list
-UserShoppingList* read_user_shopping_list() {
+/*UserShoppingList* read_user_shopping_list() {
    UserShoppingList* shoppingList = malloc(sizeof(UserShoppingList));
    memset(shoppingList, 0, sizeof(UserShoppingList));
   
@@ -216,7 +222,7 @@ UserShoppingList* read_user_shopping_list() {
    shoppingList->userPID = getpid();
    return shoppingList;
 }
-
+*/
 
 // Function to list all products in a category
 void listCategoryProducts(const char* categoryPath) {
@@ -499,6 +505,7 @@ void* searchProductInCategory(void* args){
     sem_wait(g_search_sem);
     Product* product = readProductFromFile(input->filepath);
     sem_post(g_search_sem);
+    //printf("proCount from thread : %d\n", input->proCount);
     //printf("name : %s, %s, %s\n", product->name, input->filepath, proNames[0]);
     for(int i = 0; i < input->proCount; i++){
         //printf("name : %s\n", product->name);
@@ -528,74 +535,118 @@ void* searchProductInCategory(void* args){
     return NULL;
 }
 
-void processCategories(int storeNum, const char* storePath, UserShoppingList* shoppingList) { 
-    pthread_t threads[1000]; 
-    char** categories = getSubDirectories(storePath); 
-    char** productNames = malloc(shoppingList->productCount * sizeof(char*)); 
-
-    for (int k = 0; k < shoppingList->productCount; k++) { 
-        productNames[k] = malloc(strlen(shoppingList->products[1][k].name) + 1); 
-        strcpy(productNames[k], shoppingList->products[1][k].name); 
-    } for (int i = 0; i < categoryCount; i++) { 
-        pid_t pidCategory = vfork(); 
-        if (pidCategory == 0) { 
-            //printf("category : %s\n", categories[i]); 
-            categories[i][strcspn(categories[i], "\n")] = 0; 
-            char** productFiles = getsubfiles(categories[i]); 
-            int j = 0; 
-            threadInput* inputs[500]; 
-            while (productFiles[j] != NULL) { 
-                inputs[j] = malloc(sizeof(threadInput)); 
-                inputs[j]->filepath = productFiles[j]; 
-                inputs[j]->proCount = shoppingList->productCount; 
-                inputs[j]->names = productNames; 
-                inputs[j]->product = malloc(sizeof(Product)); 
-                pthread_create(&threads[j], NULL, searchProductInCategory, (void*)inputs[j]); 
-                j++; 
-            } 
-            for (int l = 0; l < j; l++) { 
-                pthread_join(threads[l], NULL); 
-                if (inputs[l]->product->foundFlag == 1) { 
-                    //printf("%d : found product: %s in %s\n", storeNum, shoppingList->products[1][l].name, categories[i]); 
-                    
-                    memcpy(&(shoppingList->products[storeNum][inputs[l]->proNum]), inputs[l]->product, sizeof(Product)); 
-                } 
-                free(inputs[l]->product); 
-                free(inputs[l]); 
-            } 
-            exit(0); 
-            } else if (pidCategory < 0) {
-                perror("Failed to fork for category\n"); 
+void processCategories(int storeNum, const char* storePath, UserShoppingList* shoppingList) {
+    pthread_t threads[1000];
+    char** categories = getSubDirectories(storePath);
+    char** productNames = malloc(shoppingList->productCount * sizeof(char*));
+    for (int k = 0; k < shoppingList->productCount; k++) {
+        printf("proName : %s\n", shoppingList->products[1][k].name);
+        productNames[k] = malloc(strlen(shoppingList->products[1][k].name) + 1);
+        strcpy(productNames[k], shoppingList->products[1][k].name);
+    }
+    int shmFd = shoppingList->shmFd;
+    for (int i = 0; i < categoryCount; i++) {
+        pid_t pidCategory = fork();
+        if (pidCategory == 0) { // Child process
+            // Remap shared memory
+            printf("im in categories\n");
+            UserShoppingList *shoppingList = mmap(NULL, sizeof(UserShoppingList) * 10 + sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0);
+            if (shoppingList == MAP_FAILED) {
+                perror("mmap failed in child");
+                exit(EXIT_FAILURE);
             }
-        } // Free memory 
-        for (int k = 0; k < shoppingList->productCount; k++) { 
-            free(productNames[k]); 
-            } 
-        free(productNames); 
-    }  
+            categories[i][strcspn(categories[i], "\n")] = 0;
+            char** productFiles = getsubfiles(categories[i]);
 
-void processStores(UserShoppingList* shoppingList){ //making process for stores
-    char** stores = getSubStoreDirectories("Dataset");
+            int j = 0;
+            threadInput* inputs[500];
+            while (productFiles[j] != NULL) {
+                inputs[j] = malloc(sizeof(threadInput));
+                inputs[j]->filepath = productFiles[j];
+                inputs[j]->proCount = shoppingList->productCount;
+                inputs[j]->names = productNames;
+                inputs[j]->product = malloc(sizeof(Product));
 
-   for(int i = 0; i < storeCount; i++){
-       pid_t pidStore = vfork();
-       if(pidStore == 0){
-           //printf("processing store: %s\n",stores[i]);
-           stores[i][strcspn(stores[i], "\n")] = 0;
-           processCategories(i, stores[i], shoppingList);
-           exit(0);
-       }
-       else if(pidStore < 0){
-           perror("Failed to fork for store\n");
-       }
-       printf("store %d finished\n",i+1);
-   }
-   for(int i = 0; i < storeCount; i++){
-       free(stores[i]);
-   }
-   free(stores);
+                if (pthread_create(&threads[j], NULL, searchProductInCategory, (void*)inputs[j]) != 0) {
+                    perror("pthread_create failed");
+                    exit(EXIT_FAILURE);
+                }
+                j++;
+            }
+
+            for (int l = 0; l < j; l++) {
+                pthread_join(threads[l], NULL);
+                if (inputs[l]->product->foundFlag == 1) {
+                    memcpy(&(shoppingList->products[storeNum][inputs[l]->proNum]), inputs[l]->product, sizeof(Product));
+                }
+                free(inputs[l]->product);
+                free(inputs[l]);
+            }
+
+            munmap(shoppingList, sizeof(UserShoppingList) * 10 + sizeof(int));
+            exit(0);
+        } else if (pidCategory < 0) {
+            perror("Failed to fork for category\n");
+        }
+    }
+
+    // Wait for all child processes
+    for (int k = 0; k < categoryCount; k++) {
+        wait(NULL);
+    }
+
+    // Free memory
+    for (int k = 0; k < shoppingList->productCount; k++) {
+        free(productNames[k]);
+    }
+    free(productNames);
 }
 
+
+void processStores(UserShoppingList* shoppingList) {
+    char** stores = getSubStoreDirectories("Dataset");
+    int shmFd = shoppingList->shmFd; // File descriptor for shared memory
+    //printf("budget from child : %f\n", shoppingList->budgetCap);
+    for (int i = 0; i < storeCount; i++) {
+        pid_t pidStore = fork();
+
+        if (pidStore == 0) {  // Child process
+            // Map shared memory in the child process
+            UserShoppingList *mappedList = mmap(NULL, sizeof(UserShoppingList), PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0);
+            if (mappedList == MAP_FAILED) {
+                perror("mmap failed in child");
+                exit(EXIT_FAILURE);
+            }
+
+            // Debug print to verify shared memory access
+            printf("Child process for store %d: budgetCap = %f\n", i + 1, mappedList->budgetCap);
+
+            // Process the store categories
+            stores[i][strcspn(stores[i], "\n")] = 0; // Remove trailing newline
+            processCategories(i, stores[i], mappedList);
+
+            // Cleanup in child process
+            munmap(mappedList, sizeof(UserShoppingList)); // Unmap shared memory
+            exit(0); // Exit the child process
+        }
+        else if (pidStore < 0) {  // Fork failed
+            perror("Failed to fork for store");
+            break;
+        } else {  // Parent process
+            printf("Parent: forked child for store %d\n", i + 1);
+        }
+    }
+
+    // Wait for all child processes to complete
+    int status;
+    while (wait(&status) > 0); // Wait for any child process to finish
+
+    // Free allocated memory for store names
+    for (int i = 0; i < storeCount; i++) {
+        free(stores[i]);
+    }
+    free(stores);
+}	
 
 
 void processUser(UserShoppingList* shoppingList){
@@ -662,30 +713,94 @@ void processUser(UserShoppingList* shoppingList){
    sem_unlink(SEM_SHOPPING_LIST);
 }
 
-int main(){
-   while (1) {
-       pid_t pidUser = vfork(); //process user
+UserShoppingList read_user_shopping_list() {
+    UserShoppingList shoppingList;
+    memset(&shoppingList, 0, sizeof(UserShoppingList));
+ 
+    printf("Enter User ID: ");
+    scanf("%99s", shoppingList.userID);
+ 
+    printf("Enter number of products: ");
+    scanf("%d", &shoppingList.productCount);
+    printf("Order list: \n");
+    for (int i = 0; i < shoppingList.productCount; i++) {
+        printf("Product %d Name: ", i + 1);
+        scanf("%99s", shoppingList.products[1][i].name);
+        printf("Product %d Quantity: ", i + 1);
+        scanf("%d", &shoppingList.products[1][i].entity);
+    }
+    printf("Enter Budget Cap (-1 for no cap): ");
+    scanf("%lf", &shoppingList.budgetCap);
 
-       if(pidUser < 0){
-           perror("Failed to fork for User\n");
-           break;
-       }
-       else if(pidUser == 0){
-            pthread_t threads[storeCount];
-            UserShoppingList* shoppingList = read_user_shopping_list();
-            /*if(pthread_create(&threads[storeCount], NULL, calculateStoreBaskettValue, (void*)shoppingList != 0)){
-                perror("faile to create basekt value");
-                exit(1);
-            }*/
-            processUser(shoppingList);
-                // show user its choices
-            //pthread_create(&threads[1], NULL, &getProductsValue, shoppingList->products);
-            free(shoppingList);
-            exit(0);
-       }
-   }
-
-
-   printf("Exiting...\n");
-   return 0;
+    shoppingList.userPID = getpid(); // Save the process ID
+    return shoppingList;
 }
+
+int main() {
+    const char *shmName = "sharedShoppingList";
+    const int shmSize = sizeof(UserShoppingList) * 10 + sizeof(int); // Enough for 10 users and counter
+    int shmFd;
+
+    // Create shared memory in the parent process
+    shmFd = shm_open(shmName, O_CREAT | O_RDWR, 0666);
+    if (shmFd == -1) {
+        perror("shm_open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set the size of shared memory
+    if (ftruncate(shmFd, shmSize) == -1) {
+        perror("ftruncate failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Map shared memory into the parent process
+    void *sharedMem = mmap(NULL, shmSize, PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0);
+    if (sharedMem == MAP_FAILED) {
+        perror("mmap failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize shared memory
+    memset(sharedMem, 0, shmSize); // Clear shared memory
+    shoppingList = (UserShoppingList *)sharedMem; // Array of UserShoppingList structures
+    userCount = (int *)((char *)sharedMem + sizeof(UserShoppingList) *10); // Counter at the end of the shared memory
+
+    // Main loop to handle user processes
+    while (1) {
+        pid_t pid = fork(); // Create a new process for each user
+        if (pid < 0) {
+            perror("Failed to fork for User\n");
+            break;
+        } else if (pid == 0) {
+            // Child process
+            int currentUserIndex = *userCount;
+            (*userCount)++; // Increment the shared user count
+
+            UserShoppingList *currentUser = &shoppingList[currentUserIndex];
+            *currentUser = read_user_shopping_list(); // Read directly into shared memory
+            currentUser->shmFd = shmFd;
+
+            processUser(currentUser);
+            exit(0); // Exit child process
+        } else {
+            // Parent process waits for the child to finish
+            wait(NULL);
+
+            // Exit condition for demonstration purposes
+            if (*userCount >= 10) {
+                printf("Maximum user count reached.\n");
+                break;
+            }
+        }
+    }
+
+    printf("Exiting...\n");
+
+    // Clean up shared memory
+    munmap(sharedMem, shmSize);
+    shm_unlink(shmName);
+
+    return 0;
+}
+
